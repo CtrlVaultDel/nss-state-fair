9ebd40848da835aae36791ffeab09459895301f6
CtrlVaultDel <ctrlvaultdel51@gmail.com>
2020-12-02 00:00:18 -0600

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,6 @@
+// Import and invoke the ticket booth component function
+import { ticketBooth } from "./ticketBooth.js";
+import { rideTicketHolders } from "./rides/rideTicketHolders.js";
+
+ticketBooth();
+rideTicketHolders();
---
+++ scripts/rides/rideTicketHolders.js
@@ -0,0 +1,8 @@
+const contentTarget = document.querySelector(".rides")
+const eventHub = document.querySelector("#state-fair")
+
+export const rideTicketHolders = () => {
+    eventHub.addEventListener("rideTicketPurchased", customEvent => contentTarget.innerHTML +=`
+        <div class="person rider"></div>
+    `)
+}
